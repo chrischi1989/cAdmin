87209c3d0b6c7efa37707b2a09883d7afdaafaa0
Christian Wederka <info@christian-wederka.de>
2019-08-19 23:28:29 +0200

    Completed user store task

--- app/Modules/User/Tasks/StoreUserTask.php
+++ app/Modules/User/Tasks/StoreUserTask.php
@@ -2,7 +2,6 @@
 
 namespace psnXT\Modules\User\Tasks;
 
-use Carbon\Carbon;
 use psnXT\Modules\User\Models\User;
 use Ramsey\Uuid\Uuid;
 
@@ -33,17 +32,23 @@ class StoreUserTask
      */
     public function run($data = [])
     {
-        $this->user->uuid             = Uuid::uuid4();
-        $this->user->tenant_uuid      = $data['tenant_uuid'] ?? null;
-        $this->user->created_uuid     = $data['created_uuid'] ?? $this->user->uuid;
-        $this->user->updated_uuid     = $data['updated_uuid'] ?? $this->user->uuid;
-        $this->user->activated_at     = $data['activated_at'] ?? null;
-        $this->user->activated_uuid   = $data['activated_uuid'] ?? null;
-        $this->user->deactivated_at   = $data['deactivated_at'] ?? null;
-        $this->user->deactivated_uuid = $data['deactivated_uuid'] ?? null;
-        $this->user->lastlogin_at     = $data['lastlogin_at'] ?? null;
-        $this->user->email            = $data['email'];
-        $this->user->
+        $this->user->uuid                  = Uuid::uuid4();
+        $this->user->tenant_uuid           = $data['tenant_uuid'] ?? null;
+        $this->user->created_uuid          = $data['created_uuid'] ?? $this->user->uuid;
+        $this->user->updated_uuid          = $data['updated_uuid'] ?? $this->user->uuid;
+        $this->user->activated_at          = $data['activated_at'] ?? null;
+        $this->user->activated_uuid        = $data['activated_uuid'] ?? (!is_null($this->user->activated_at ? $this->user->uuid : null));
+        $this->user->deactivated_at        = $data['deactivated_at'] ?? null;
+        $this->user->deactivated_uuid      = $data['deactivated_uuid'] ?? null;
+        $this->user->lastlogin_at          = $data['lastlogin_at'] ?? null;
+        $this->user->email                 = $data['email'];
+        $this->user->password              = $data['password'];
+        $this->user->activation_token      = $data['activation_token'] ?? null;
+        $this->user->failed_logins         = $data['failed_logins'] ?? 0;
+        $this->user->failed_logins_max     = $data['failed_logins_max'] ?? 5;
+        $this->user->password_expires      = $data['password_expires'] ?? true;
+        $this->user->password_expires_days = $data['password_expires_days'] ?? 90;
+
 
         return $this->user->save();
     }
