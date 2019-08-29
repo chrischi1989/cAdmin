c33e6911110a8fc236fa92f1db3f7dcd3be3b33b
Christian Wederka <wederka@psnmedia.de>
2019-08-29 16:53:47 +0200

    Seeding issue

--- app/Modules/Install/Tasks/SeedTablesTask.php
+++ app/Modules/Install/Tasks/SeedTablesTask.php
@@ -14,8 +14,20 @@ class SeedTablesTask
      * @return bool
      */
     public function run() {
-        foreach(scandir(app_path('Modules')) as $module) {
-            if($module != '.' && $module != '..' && file_exists(app_path('Modules/' . $module . '/Data/Seeders/DatabaseSeeder.php'))) {
+        $seedOrder = [
+            'Dashboard',
+            'Install',
+            'Module',
+            'Navigation',
+            'Page',
+            'Setting',
+            'Tenant',
+            'User',
+            'Accesslaser'
+        ];
+
+        foreach($seedOrder as $module) {
+            if(file_exists(app_path('Modules/' . $module . '/Data/Seeders/DatabaseSeeder.php'))) {
                 Artisan::call('db:seed', ['--class' => 'psnXT\Modules\\' . $module . '\Data\Seeders\DatabaseSeeder']);
             }
         }
