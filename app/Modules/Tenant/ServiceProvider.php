6530b9cb5c3f491deb70febcf26f9608d1f92039
Christian Wederka <wederka@psnmedia.de>
2019-08-30 13:34:26 +0200

    Added policy checking

--- app/Modules/Tenant/ServiceProvider.php
+++ app/Modules/Tenant/ServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace psnXT\Modules\Tenant;
 
+use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use psnXT\Modules\Tenant\Models\Tenant;
+use psnXT\Modules\Tenant\Policies\TenantPolicy;
 
 class ServiceProvider extends BaseServiceProvider
 {
@@ -11,12 +14,14 @@ class ServiceProvider extends BaseServiceProvider
 
     }
 
-    public function boot()
+    public function boot(Gate $gate)
     {
         $this->loadRoutesFrom(__DIR__ . '/module.php');
 
         $this->loadMigrationsFrom(__DIR__ . '/Data/Migrations');
 
         $this->loadViewsFrom(__DIR__ . '/UI/Web/Views', 'tenant');
+
+        $gate->policy(Tenant::class, TenantPolicy::class);
     }
 }
