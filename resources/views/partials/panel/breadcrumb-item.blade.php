0b3e69ada16b6c36d3a4ca0e4cdd113659ddd433
Christian Wederka <info@christian-wederka.de>
2019-09-02 00:25:59 +0200

    Removed icon from breadcrumb

--- resources/views/partials/panel/breadcrumb-item.blade.php
+++ resources/views/partials/panel/breadcrumb-item.blade.php
@@ -1,9 +1,9 @@
 <li class="breadcrumb-item{{ isset($item['active']) ? ' active' : null }}">
     @isset($item['active'])
-    <span class="fa-fw {{ $item['icon'] }}"></span> {{ $item['title'] }}
+    {{ $item['title'] }}
     @else
     <a href="{{ $item['href'] }}">
-        <span class="fa-fw {{ $item['icon'] }}"></span> {{ $item['title'] }}
+        {{ $item['title'] }}
     </a>
     @endisset
 </li>
