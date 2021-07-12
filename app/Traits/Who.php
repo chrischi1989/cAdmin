afbe9a7ffbdd2b912552eb14006d0faf62295203
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2021-07-12 15:55:20 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- app/Traits/Who.php
+++ app/Traits/Who.php
@@ -5,11 +5,13 @@ use Modules\User\Models\User;
 
 trait Who
 {
-    public function createdBy() {
+    public function createdBy()
+    {
         return $this->belongsTo(User::class, 'created_uuid', 'uuid');
     }
 
-    public function updatedBy() {
+    public function updatedBy()
+    {
         return $this->belongsTo(User::class, 'updated_uuid', 'uuid');
     }
 }
